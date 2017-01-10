4aaeb0891708b6395183af79bfaa6eb7b8428d9f
dariano <darianosoares@hotmail.com>
2017-01-10 01:03:31 -0200

    Ajustando a configuração do mongodb

--- config/config.js
+++ config/config.js
@@ -1,5 +1,14 @@
-module.exports = function() {
+module.exports = function () {
+	let MONGO_DB;
+	let FIG_DB = process.env.DB_1_PORT;
+	
+	if (FIG_DB) {
+		MONGO_DB = FIG_DB.replace("tcp", "mongodb") + "/dev_db";
+	} else {
+		MONGO_DB = process.env.MONGO_URL;
+	}
+
 	return {
-		db: 'mongodb://localhost:27017/desafio-sp'
+		db: MONGO_DB
 	};
 };
--- config/database.js
+++ config/database.js
@@ -1,29 +1,21 @@
 var mongoose = require('mongoose');
 
 module.exports = function (uri) {
-	//mongoose.connect(uri);
-	var MONGO_DB;
-	var FIG_DB = process.env.DB_1_PORT;
-	if (FIG_DB) {
-		MONGO_DB = FIG_DB.replace("tcp", "mongodb") + "/dev_db";
-	} else {
-		MONGO_DB = process.env.MONGO_URL;
-	}
-	mongoose.connect(MONGO_DB);
-
+	mongoose.connect(uri);
+	
 	mongoose.connection.on('connected', function () {
 		console.log('Mongoose! Conectado em ' + uri);
 	});
-
+	
 	mongoose.connection.on('disconnected', function () {
 		console.log('Mongoose! Desconectado de ' + uri);
 	});
-
+	
 	/* global process  */
 	process.on('SIGINT', function () {
 		mongoose.connection.close(function () {
 			console.log('Mongoose! Desconectado pelo término da aplicação');
-
+			
 			process.exit(0);
 		});
 	});
